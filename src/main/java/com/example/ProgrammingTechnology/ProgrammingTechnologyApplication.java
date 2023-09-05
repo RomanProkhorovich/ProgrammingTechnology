f25d5d714b80c88b00c670391176cd9bd0df5231
RomanProkhorovich <pomaprokhorovich583@gmail.com>
2023-09-06 03:35:44 +0400

    Initial commit

---
+++ src/main/java/com/example/ProgrammingTechnology/ProgrammingTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgrammingTechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingTechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingTechnologyApplication.class, args);
+	}
+
+}
