0a2dce5f2a22739b547d22b78a1d983f9ceca1b0
RomanProkhorovich <pomaprokhorovich583@gmail.com>
2023-09-08 22:12:11 +0400

    bugfix

--- src/main/resources/db/changelog/changeset/create-Order-table.sql
+++ src/main/resources/db/changelog/changeset/create-Order-table.sql
@@ -3,9 +3,9 @@ CREATE TABLE app_Order
 (
 	id SERIAL PRIMARY KEY,
 	order_time TIMESTAMP NOT NULL,
-	status_id INTEGER REFERENCES Status (id) UNIQUE,
+	status_id INTEGER REFERENCES Status (id) ,
 	delivery_time TIMESTAMP DEFAULT NULL,
-	courier_id INTEGER REFERENCES Customer (id) UNIQUE,
-	client_id INTEGER REFERENCES Customer (id) UNIQUE,
+	courier_id INTEGER REFERENCES Customer (id) ,
+	client_id INTEGER REFERENCES Customer (id) ,
 	is_offline BOOLEAN NOT NULL
 );
