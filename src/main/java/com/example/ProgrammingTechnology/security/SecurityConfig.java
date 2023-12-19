8ac6f465c34ae746076907e6b15dad47b31b65a8
Viktoria <viktoriia.andreeva@gmail.com>
2023-12-19 19:55:30 +0400

    filter

--- src/main/java/com/example/ProgrammingTechnology/security/SecurityConfig.java
+++ src/main/java/com/example/ProgrammingTechnology/security/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.header.HeaderWriterFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -52,11 +58,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> cors.disable())
-                .csrf(AbstractHttpConfigurer::disable)
+                //.cors(cors -> cors.disable())
+                .addFilterAt(new Filter(), CorsFilter.class)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/*auth/**").permitAll()
                         .anyRequest().authenticated())
                 .build();
     }
