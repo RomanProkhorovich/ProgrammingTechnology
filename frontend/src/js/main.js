8d5d3ff905a8defc84a30345b0b20d3f61cd125b
gzdewww <gzdeww@yandex.ru>
2023-12-21 17:05:12 +0400

    cart works

---
+++ frontend/src/js/main.js
@@ -0,0 +1,229 @@
+import menu from "./menu.js";
+
+export default class Header {
+  constructor() {
+    this.accountButton = document.querySelector(".header-account");
+    this.cartButton = document.querySelector(".header-cart-logo");
+    this.cartSum = document.querySelector(".header-cart-sum-value");
+    this.cartContent = document.querySelector(".header-cart-content");
+    this.cartSummary = document.querySelector("#cart-to-order");
+    this.cartDishes;
+
+    if (localStorage.getItem("Cart")) {
+      this.cartDishes = JSON.parse(localStorage.getItem("Cart"));
+    }
+
+    this.registerEvents();
+  }
+
+  // CART CALC
+  calcCart() {
+    let arr = [];
+    this.cartContent
+      .querySelectorAll(".header-cart-content-dish")
+      .forEach((item) => {
+        arr.push({
+          id: item.dataset.cartId,
+          quantity: +item.querySelector(".quantity-value").value,
+          price: +item.querySelector(".content-menu-dish-cart-price-value")
+            .textContent,
+          // price: Array.from(JSON.parse(localStorage.getItem("Menu"))).find(
+          //   (item) => {
+          //     item.id === item.dataset.cartId;
+          //   }
+          // ).price,
+        });
+      });
+    this.cartSum.textContent = arr.reduce((sum, current) => {
+      return sum + current.price * current.quantity;
+    }, 0);
+
+    localStorage.setItem(
+      "Cart",
+      JSON.stringify(
+        arr.filter((item) => {
+          return item.quantity > 0;
+        })
+      )
+    );
+  }
+
+  renderCart() {
+    if (!this.cartDishes) return;
+
+    this.cartDishes.forEach((dish) => {
+      const curDish = Array.from(JSON.parse(localStorage.getItem("Menu"))).find(
+        (item) => {
+          return item.id === dish.id;
+        }
+      );
+
+      document.querySelector("#cart-to-order").insertAdjacentHTML(
+        "beforebegin",
+        `<div class="header-cart-content-dish" data-cart-id="${dish.id}">
+    <img class="header-cart-content-dish-photo" src="data:image/png;base64,${curDish.photo}" alt="${curDish.name}">
+    <div class="header-cart-content-dish-info">
+        <h1>${curDish.name}</h1>
+        <p class="header-cart-content-dish-info-quantity">
+          <div class="quantity" data-quantity-id="${dish.id}">
+            <span class="quantity-control minus">-</span><input class="quantity-value"
+            value="${dish.quantity}"></input><span class="quantity-control plus">+</span>
+          </div>
+        </p>
+        <p class="header-cart-content-dish-info-price"><span
+            class="content-menu-dish-cart-price-value">${dish.price}</span>р.</p>
+    </div>
+  </div>`
+      );
+      this.calcCart();
+    });
+  }
+
+  // REG/AUTH DATA SEND XHR
+  authSendRequest(authForm) {
+    let params = {};
+
+    Array.from(authForm.getElementsByTagName("input")).map((item) => {
+      params[item.name] = item.value;
+    });
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "POST",
+      `http://localhost:8080/auth${
+        authForm.id === "authorization-form" ? "" : "/reg"
+      }`
+    );
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4 || xhr.status !== 200) {
+        return;
+      }
+      localStorage.setItem("Auth", 1);
+      console.log(JSON.parse(xhr.responseText));
+    };
+    xhr.send(JSON.stringify(params));
+
+    authForm.closest(".popup").remove();
+  }
+
+  // SWITCH TO REGISTRATION
+  popupRegForm() {
+    const popup = document.querySelector(".popup");
+    popup.innerHTML = `<form id="registration-form">
+    <h1>
+        Регистрация
+    </h1>
+    <input type="text" placeholder="Фамилия" name="lastname" value="">
+    <input type="text" placeholder="Имя" name="firstname" value="">
+    <input type="text" placeholder="Отчество (при наличии)" name="surname" value="">
+    <input type="text" placeholder="E-mail" name="email" value="">
+    <input type="text" placeholder="Номер телефона" name="phone" value="">
+    <input type="password" placeholder="Пароль" name="password" value="">
+    <button id="registration-button" type="button">Зарегистрироваться</button>
+  </form>
+  `;
+    popup
+      .querySelector("#registration-button")
+      .addEventListener("click", (e) =>
+        this.authSendRequest(e.target.closest("form"))
+      );
+  }
+
+  // AUTH POPUP
+  popupAuthForm() {
+    document.body.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="popup hidden">
+      <form id="authorization-form">
+          <h1>
+              Войдите, чтобы сделать заказ
+          </h1>
+          <input type="text" placeholder="Номер телефона или E-Mail" name="phone-email" value="">
+          <input type="password" placeholder="Пароль" name="password" value="">
+          <button id="registration-button" type="button">Зарегистрироваться</button>
+          <button id="authorization-button" type="button">Войти</button>
+      </form>
+  </div>`
+    );
+    const popup = document.querySelector(".popup");
+    setTimeout(() => {
+      popup.classList.remove("hidden");
+    }, 0);
+    popup
+      .querySelector("#registration-button")
+      .addEventListener("click", () => this.popupRegForm());
+    popup
+      .querySelector("#authorization-button")
+      .addEventListener("click", (e) =>
+        this.authSendRequest(e.target.closest("form"))
+      );
+  }
+
+  registerEvents() {
+    // HIDE POPUP
+    document.body.addEventListener("click", (e) => {
+      if (e.target.classList.contains("popup")) {
+        e.target.classList.add("hidden");
+        setTimeout(() => {
+          e.target.remove();
+        }, 200);
+      }
+    });
+    // ACCOUNT LISTENER
+    this.accountButton.addEventListener("click", () => {
+      if (!localStorage.getItem("Auth")) this.popupAuthForm();
+      else {
+        window.location.href = "./src/html/cabinet.html";
+      }
+    });
+    // SHOW/HIDE CART
+    this.cartButton.addEventListener("click", () => {
+      const cartContent = document.querySelector(".header-cart-content");
+      if (cartContent.classList.contains("display-none")) {
+        cartContent.classList.toggle("display-none");
+        setTimeout(() => {
+          cartContent.classList.toggle("hidden");
+        }, 0);
+      } else {
+        cartContent.classList.toggle("hidden");
+        setTimeout(() => {
+          cartContent.classList.toggle("display-none");
+        }, 200);
+      }
+    });
+    // CART QUANTITY LISTENER
+    this.cartContent.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("quantity-control")) return;
+
+      const value = e.target.closest(".quantity").querySelector("input");
+
+      if (e.target.classList.contains("plus")) {
+        value.value++;
+        value.dispatchEvent(new Event("change", { bubbles: true }));
+        return;
+      }
+      value.value--;
+      value.dispatchEvent(new Event("change", { bubbles: true }));
+    });
+    this.cartContent.addEventListener("change", (e) => {
+      const quantity = e.target.value;
+      const id = e.target.closest(".quantity").dataset.quantityId;
+      document
+        .querySelectorAll(`[data-quantity-id="${id}"]`)
+        .forEach((item) => {
+          const value = item.querySelector("input");
+          value.value = quantity;
+        });
+      if (quantity < 1) {
+        e.target.closest(".header-cart-content-dish").remove();
+      }
+      this.calcCart();
+    });
+    // TO SUMMARY PAGE
+    this.cartSummary.addEventListener("click", () => {
+      window.location.href = "./src/html/summary.html";
+    });
+    this.renderCart();
+  }
+}
--- frontend/src/js/menu.js
+++ frontend/src/js/menu.js
@@ -1,9 +1,34 @@
-class Menu {
+import header from "./main.js";
+
+export default class Menu {
   constructor() {
-    this.cartSum = document.querySelector(".header-cart-sum-value");
+    this.cartContent = document.querySelector(".header-cart-content");
     this.dishes = [];
 
-    this.getDishes();
+    this.getDishesLS();
+  }
+
+  getDishesLS() {
+    const dishes = JSON.parse(localStorage.getItem("Menu"));
+
+    const dishesContainer = document.querySelector(".content-menu");
+
+    dishes.forEach((item) => {
+      dishesContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="content-menu-dish" data-menu-id="${item.id}">
+        <img class="content-menu-dish-photo" src="data:image/png;base64,${item.photo}" alt="${item.name}">
+        <h1>${item.name}</h1>
+        <p class="content-menu-dish-description">${item.description}</p>
+        <p class="content-menu-dish-cart-price">
+          <span class="content-menu-dish-cart-price-value">${item.price}</span>р.
+        </p>
+        <div class="content-menu-dish-cart-quantity">
+            <button class="content-menu-dish-cart-button">В корзину</button>
+        </div>
+    </div>`
+      );
+    });
     this.addToCart();
   }
   // DISHES RENDERING XHR
@@ -45,16 +70,66 @@ class Menu {
     };
     xhr.send();
   }
+  toCartButton(elem) {
+    const parent = elem.parentElement;
+
+    elem.outerHTML =
+      '<button class="content-menu-dish-cart-button">В корзину</button>';
+    parent.querySelector(".content-menu-dish-cart-button").addEventListener();
+  }
+  // SWITCH TO QUANTITY CONTROL
+  toQuantity(elem, id, quantity) {
+    const parent = elem.parentElement;
+
+    elem.outerHTML = `<div class="quantity" data-quantity-id="${id}">
+              <span class="quantity-control minus">-</span><input class="quantity-value"
+                  value="${quantity}"></input><span class="quantity-control plus">+</span>
+          </div>`;
+
+    const plus = parent.querySelector(".plus");
+    const minus = parent.querySelector(".minus");
+
+    plus.addEventListener("click", (e) => {
+      const value = this.cartContent.querySelector(
+        `[data-quantity-id="${id}"] input`
+      );
+      value.value++;
+      value.dispatchEvent(new Event("change", { bubbles: true }));
+    });
+
+    minus.addEventListener("click", (e) => {
+      const value = this.cartContent.querySelector(
+        `[data-quantity-id="${id}"] input`
+      );
+      value.value--;
+      value.dispatchEvent(new Event("change", { bubbles: true }));
+    });
+  }
   // ADD DISH TO CART LISTENER
   addToCart() {
     document
       .querySelectorAll(".content-menu-dish-cart-button")
       .forEach((item) => {
+        const dish = item.closest(".content-menu-dish");
+        const id = dish.dataset.menuId;
+        // already in cart
+        try {
+          const fromCart = JSON.parse(localStorage.getItem("Cart")).find(
+            (item) => {
+              return item.id === id;
+            }
+          );
+          if (fromCart) {
+            this.toQuantity(item, id, fromCart.quantity);
+            return;
+          }
+        } catch (E) {
+          console.log("cart is empty");
+        }
+        // not in cart
         item.addEventListener("click", (e) => {
           e.preventDefault();
 
-          const dish = e.target.closest(".content-menu-dish");
-          const id = dish.dataset.menuId;
           const src = dish.querySelector(".content-menu-dish-photo").src;
           const alt = dish.querySelector(".content-menu-dish-photo").alt;
           const name = dish.querySelector("h1").textContent;
@@ -69,7 +144,7 @@ class Menu {
       <div class="header-cart-content-dish-info">
           <h1>${name}</h1>
           <p class="header-cart-content-dish-info-quantity">
-            <div class="quantity">
+            <div class="quantity" data-quantity-id="${id}">
               <span class="quantity-control minus">-</span><input class="quantity-value"
               value="1"></input><span class="quantity-control plus">+</span>
             </div>
@@ -79,38 +154,8 @@ class Menu {
       </div>
   </div>`
           );
-
-          this.cartSum.textContent = +this.cartSum.textContent + +price;
-
-          // switch to quantity control
-
-          const parent = item.parentElement;
-
-          item.outerHTML = `<div class="quantity">
-          <span class="quantity-control minus">-</span><input class="quantity-value"
-              value="1"></input><span class="quantity-control plus">+</span>
-      </div>`;
-
-          const plus = parent.querySelector(".plus");
-          const minus = parent.querySelector(".minus");
-          const value = parent.querySelector(".quantity-value");
-
-          plus.addEventListener("click", (e) => {
-            value.value++;
-            document
-              .querySelector(`[data-cart-id="${id}"]`)
-              .querySelector("input").value = value.value;
-          });
-
-          minus.addEventListener("click", (e) => {
-            value.value--;
-            document
-              .querySelector(`[data-cart-id="${id}"]`)
-              .querySelector("input").value = value.value;
-          });
+          this.toQuantity(item, id, 1);
         });
       });
   }
 }
-
-new Menu();
